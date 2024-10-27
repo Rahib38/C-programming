29bbf3de4596aaca31334afb1b23d019eda6a2d3
Nadimul Rahib <nadimulrahib38@gmail.com>
2024-10-26 20:01:33 -0700

    added a how to next line and tab

---
+++ Module1/print1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    printf("Hello \n");
+        //  \n => next line
+    printf("I am a programmer \n\n");
+
+
+
+    // ---------------------
+
+
+
+    // \t=> tab space{4 space}
+    printf("Hello \t I am a programmer.");
+    return 0;
+}
