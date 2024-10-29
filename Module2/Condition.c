f704fcad19b500073374545ebf12041ce518a332
Nadimul Rahib <nadimulrahib38@gmail.com>
2024-10-28 20:55:27 -0700

    added a condition1

---
+++ Module2/Condition.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main (){
+
+    int tk;
+    scanf("%d",&tk);
+    if (tk>=100)
+    {
+        printf("eating burger");
+    }
+    else{
+        printf("no eating");
+    }
+
+    
+    return 0;
+}
---
+++ Module2/Condition1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main (){
+
+    int tk;
+    scanf("%d",&tk);
+    if (tk>=100)
+    {
+        printf("eating burger");
+    }else if (tk>=50)
+    {
+        printf("eating fucka");
+    }else{
+        printf("no eating");
+    }
+    
+    
+
+    return 0;
+}
